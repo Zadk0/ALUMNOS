6b53c2cff87dda56ddac7e19072db51d20c33aa1
Martin Tellez Falcón <martintellezfalcon@gmail.com>
2026-02-08 10:44:14 -0600

    Rutas corregidas en menu.php

--- views/menu.php
+++ views/menu.php
@@ -1,8 +1,13 @@
 <nav>
     <ul>
         <li><a href="index.php">Inicio</a></li>
-        <li><a href="registro_alumno.php">Registrar Alumno</a></li>
-        <li><a href="registro_grupo.php">Registrar Grupo</a></li>
-        <li><a href="alumnos.php">Ver Alumnos (Tabla)</a></li>
+        
+        <li><a href="alumnos.php">Lista Alumnos</a></li>
+        <li><a href="grupos.php">Lista Grupos</a></li>
+        <li><a href="carreras.php">Carreras</a></li>
+        <li><a href="turnos.php">Turnos</a></li>
+        
+        <li><a href="registro_alumno.php">Nuevo Alumno</a></li>
+        <li><a href="registro_grupo.php">Nuevo Grupo</a></li>
     </ul>
 </nav>
